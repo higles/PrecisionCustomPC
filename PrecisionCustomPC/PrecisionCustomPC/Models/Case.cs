b2d4644213f58642e5d2bd5ebbcaeb6dfa308095
Joe Higley <josephhigley@gmail.com>
2017-05-01 15:18:08 -0700

    Added models for Cases and Motherboards

---
+++ PrecisionCustomPC/PrecisionCustomPC/Models/Case.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static PrecisionCustomPC.Options;
+
+namespace PrecisionCustomPC.Models
+{
+    public class Case
+    {
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0, 10000, ErrorMessage = "Price out of range")]
+        [DisplayName("Price:")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Brand is required")]
+        [MinLength(3, ErrorMessage = "Brand name too short")]
+        [DisplayName("Brand:")]
+        public string Brand { get; set; }
+
+        [MinLength(3, ErrorMessage = "Series name too short")]
+        [DisplayName("Series:")]
+        public string Series { get; set; }
+
+        [Key]
+        [MinLength(3, ErrorMessage = "Model name too short")]
+        [DisplayName("Model:")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Size is required")]
+        [DisplayName("Size:")]
+        public Sizes Size { get; set; }
+
+        [DisplayName("Colors:")]
+        public List<String> Colors { get; set; }
+
+        [Required(ErrorMessage = "Fan count is required")]
+        [Range(0, 10, ErrorMessage = "Fan count not in range")]
+        [DisplayName("Fan Count:")]
+        public int FanCount { get; set; }
+
+        [Required(ErrorMessage = "Liquid Cooling compatibility is required")]
+        [DisplayName("Liquid Cooling Compatible:")]
+        public bool LiquidCooling { get; set; }
+    }
+}
---
+++ PrecisionCustomPC/PrecisionCustomPC/Models/Motherboard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static PrecisionCustomPC.Options;
+
+namespace PrecisionCustomPC.Models
+{
+    public class Motherboard
+    {
+        [Required(ErrorMessage = "")]
+        [Range(0, 10000, ErrorMessage = "")]
+        [DisplayName("Price:")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "")]
+        [MinLength(3, ErrorMessage = "")]
+        [DisplayName("Brand:")]
+        public string Brand { get; set; }
+
+        [MinLength(3, ErrorMessage = "")]
+        [DisplayName("Series:")]
+        public string Series { get; set; }
+
+        [Key]
+        [MinLength(3, ErrorMessage = "")]
+        [DisplayName("Model:")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "")]
+        [DisplayName("Size:")]
+        public Sizes Size { get; set; }
+
+        [Required(ErrorMessage = "")]
+        [DisplayName("Maximum Memory:")]
+        public RAMSizes MaxRAM { get; set; }
+
+        [DisplayName("Memory Standard:")]
+        public RAMClasses RAMClass { get; set; }
+
+        [Required(ErrorMessage = "")]
+        [Range(0, 10, ErrorMessage = "")]
+        [DisplayName("Memory Slots:")]
+        public int RamSlots { get; set; }
+
+        [Range(0, 10, ErrorMessage = "")]
+        [DisplayName("PCI Express Slots:")]
+        public int PCISlots { get; set; }
+
+        [Range(0, 10, ErrorMessage = "")]
+        [DisplayName("USB 3.0 Slots:")]
+        public int USB3Slots { get; set; }
+    }
+}
